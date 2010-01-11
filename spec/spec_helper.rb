d5f21bba774dd73793ecb63b5152020bd8753fac
Devin Christensen <devin@threetrieslater.com>
2010-01-11 14:20:45 -0700

    Initial commit to queue_to_the_future.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'queue_to_the_future'
+require 'spec'
+require 'spec/autorun'
+
+Spec::Runner.configure do |config|
+  
+end
